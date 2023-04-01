0a927d66d545397eeae979fe7d486f66b8fa5ef1
woshizhu66 <90317315+woshizhu66@users.noreply.github.com>
2023-04-01 18:29:39 +0100

    Add PairingHeap_vs_DaryHeap experiment in testDrive

--- Assignment3/src/test/TestDrive.java
+++ Assignment3/src/test/TestDrive.java
@@ -3,10 +3,12 @@ package test;
 import java.util.ArrayList; 
 import java.util.Hashtable;
 
+import daryHeap.DaryHeap;
 import halfPrioQueue.HalfPrioQueueByArr;
 import halfPrioQueue.HalfPrioQueueByLinkedList;
 import priorityQueue.PriorityQueueByArr;
 import halfPrioQueue.HalfPrioQueueByLinkedListImp1;
+import pairingHeap.PairingHeap;
 import priorityQueue.PriorityQueueBySinglyLinkedList;
 
 import priorityQueue.PriorityQueueByLinkedListOptim;
@@ -52,6 +54,34 @@ public class TestDrive {
 		performanceTest.run();
 	}
 	
+	/**
+	Experiments wrappers.
+	*/
+	static void PairingHeap_vs_DaryHeap(int batch_size, double rate) {
+		EfficiencyTest efficiencyTest2 = new EfficiencyTest();
+		
+		efficiencyTest2.setQueue1(new PairingHeap());
+		efficiencyTest2.setQueue2(new DaryHeap(1000000, 4));
+		
+		efficiencyTest2.setDatasize(batch_size);
+		efficiencyTest2.setBatchSize(batch_size);
+
+		if(rate == 0.1) {
+			efficiencyTest2.setDatasetProbability(100, rate);
+			efficiencyTest2.setDatasetProbability(1, 1.0);
+		}
+		else if(rate==0.9){
+			efficiencyTest2.setDatasetProbability(100, 1.0);
+			efficiencyTest2.setDatasetProbability(1, 1-rate);
+		}
+		else {
+			efficiencyTest2.setDatasetProbability(100, rate);
+			efficiencyTest2.setDatasetProbability(1, 1-rate);
+		}
+		
+		efficiencyTest2.run2();
+	}
+
 	/**
 	Experiments wrappers.
 	*/
